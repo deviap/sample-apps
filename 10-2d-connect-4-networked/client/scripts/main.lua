b84b00e7dd065a5bbe5ab64bb807630ba2e46b03
utrain <skassem05@outlook.com>
2021-01-15 20:04:06 -0500

    Init connect4

---
+++ 10-2d-connect-4-networked/client/scripts/main.lua
@@ -0,0 +1,20 @@
+print("Client")
+
+--[[
+	Main menu ->
+		|> settings ->
+			|> Colors
+		|> online play
+			|> Rooms
+				|> Create new room
+				|> Join with Id
+				|> Join with menu
+		|> local play
+			|> Two-player
+			|> AI
+		|> about
+			|> utrain, 
+			|> deviap, 
+			|> source, 
+			|> tutorial
+--]]
---
+++ 10-2d-connect-4-networked/server/scripts/main.lua
@@ -0,0 +1,11 @@
+print("Server")
+
+--[[
+	Networked event:
+		|> Create new room
+			|> Player left
+			|> Player joined
+			|> Player played
+		|> Chat
+			|> Chatted
+--]]
